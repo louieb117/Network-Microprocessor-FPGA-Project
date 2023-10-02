919e280bc6cad82a0924543a960bc19e5aba7385
Luis <lbellov@outlook.com>
2023-10-01 22:03:36 -0700

    Updated alu to fit best practices formtting

---
+++ SingleCycle/SingleCycle.srcs/sim_1/new/tb_alu.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+module tb_alu;
+
+    // Word Lengths 
+    parameter AWL = 32;
+    parameter DWL = 16;
+    
+        
+    // Control Signals
+    reg CLK; 
+    reg DMWE;
+    
+    // Input Signals
+    reg [AWL-1:0] DMA;
+    reg [DWL-1:0] DMWD;
+    
+    // Output Signals
+    wire [DWL-1:0] DMRD;
+    
+    // Clock Generation
+    initial CLK <= 0;
+        always #10 CLK <= ~CLK;
+    
+    // Module Under Test
+    data_memory DM01(
+        .CLK(CLK), 
+        .DMWE(DMWE), 
+        .DMA(DMA),
+        .DMWD(DMWD),
+        .DMRD(DMRD)
+    );
+    
+    // Test Bench
+    initial begin 
+
+        // load word
+        @(posedge CLK) begin
+            DMA = 32'd4;          
+            DMWD = 16'd64;          
+            DMWE = 1;
+        end
+        @(posedge CLK);
+                
+        // load word
+        @(posedge CLK) begin
+            DMA = 32'd8;          
+            DMWD = 16'd256;          
+            DMWE = 1;
+        end
+        @(posedge CLK);
+        
+        // store word
+        @(posedge CLK) begin
+            DMA = 32'd4;          
+            DMWD = 16'd00;          
+            DMWE = 0;
+        end
+        
+        // store word
+        @(posedge CLK) begin
+            DMA = 32'd8;          
+            DMWD = 16'd00;          
+            DMWE = 0;
+        end
+        
+        @(posedge CLK);
+    end    
+endmodule
--- SingleCycle/SingleCycle.srcs/sources_1/new/alu.v
+++ SingleCycle/SingleCycle.srcs/sources_1/new/alu.v
@@ -1,22 +1,20 @@
 `timescale 1ns / 1ps 
 
-module alu #(parameter IWL = 32,
-                       SWL = 3,
-                       OWL = 32,
-                       ADD = 3'b000,
-                       SUB = 3'b001,
-                       MUL = 3'b010,
-                       DIV = 3'b011)
-    
-    ( A, B, ALU_Sel, ALU_Out, ALU_CARRY); 
-    
-    input signed [IWL-1:0] A;
-    input signed [IWL-1:0] B;               
-    input [SWL-1:0] ALU_Sel;
-    output signed [OWL-1:0] ALU_Out;
-    output reg ALU_CARRY;
-
-    reg [OWL-1:0] ALU_Out;
+module alu #(
+    parameter   IWL = 32,
+    parameter   SWL = 3,
+    parameter   OWL = 32,
+    parameter   ADD = 3'b000,
+    parameter   SUB = 3'b001,
+    parameter   MUL = 3'b010,
+    parameter   DIV = 3'b011
+)(     
+    input signed [IWL-1:0] A,
+    input signed [IWL-1:0] B,
+    input [SWL-1:0] ALU_Sel,
+    output reg [OWL-1:0] ALU_Out,
+    output reg ALU_CARRY
+);
 
     always @( * )begin
         case(ALU_Sel)
